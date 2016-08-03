5143007dc245d1c52e0666b506b2c91f65f1b47f
takecare <rui@vazteixeira.org>
2016-08-03 19:06:58 +0100

    declare assets

--- js/game.js
+++ js/game.js
@@ -6,3 +6,11 @@ var gameProperties = {
 var states = {
     game: "game",
 };
+
+var graphicAssets = {
+    ship: {URL:'assets/ship.png', name:'ship'},
+    bullet: {URL:'assets/bullet.png', name:'bullet'},
+    asteroidLarge: {URL:'assets/asteroidLarge.png', name:'asteroidLarge'},
+    asteroidMedium: {URL:'assets/asteroidMedium.png', name:'asteroidMedium'},
+    asteroidSmall: {URL:'assets/asteroidSmall.png', name:'asteroidSmall'},
+};
