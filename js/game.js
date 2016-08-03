10961e7e60d84ecab524d3cc28af0bd7e9f0b8c7
takecare <rui@vazteixeira.org>
2016-08-03 19:12:04 +0100

    define and load assets

--- js/game.js
+++ js/game.js
@@ -8,9 +8,29 @@ var states = {
 };
 
 var graphicAssets = {
-    ship: {URL:'assets/ship.png', name:'ship'},
-    bullet: {URL:'assets/bullet.png', name:'bullet'},
-    asteroidLarge: {URL:'assets/asteroidLarge.png', name:'asteroidLarge'},
-    asteroidMedium: {URL:'assets/asteroidMedium.png', name:'asteroidMedium'},
-    asteroidSmall: {URL:'assets/asteroidSmall.png', name:'asteroidSmall'},
+  ship: {URL:'assets/ship.png', name:'ship'},
+  bullet: {URL:'assets/bullet.png', name:'bullet'},
+  asteroidLarge: {URL:'assets/asteroidLarge.png', name:'asteroidLarge'},
+  asteroidMedium: {URL:'assets/asteroidMedium.png', name:'asteroidMedium'},
+  asteroidSmall: {URL:'assets/asteroidSmall.png', name:'asteroidSmall'},
 };
+
+var shipProperties = {
+    startX: gameProperties.screenWidth / 2,
+    startY: gameProperties.screenHeight / 2,
+};
+
+var gameState = function (game) {
+  this.shipSprite;
+};
+
+gameState.prototype = {
+
+  preload: function () {
+      game.load.image(graphicAssets.asteroidLarge.name, graphicAssets.asteroidLarge.URL);
+      game.load.image(graphicAssets.asteroidMedium.name, graphicAssets.asteroidMedium.URL);
+      game.load.image(graphicAssets.asteroidSmall.name, graphicAssets.asteroidSmall.URL);
+      game.load.image(graphicAssets.bullet.name, graphicAssets.bullet.URL);
+      game.load.image(graphicAssets.ship.name, graphicAssets.ship.URL);
+  }
+}
