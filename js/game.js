89692c745f4c869505b6af0df4eeeec40a5d3a1d
takecare <rui@vazteixeira.org>
2016-08-03 19:20:48 +0100

    load the ship sprite and init the game

--- js/game.js
+++ js/game.js
@@ -25,12 +25,33 @@ var gameState = function (game) {
 };
 
 gameState.prototype = {
-
   preload: function () {
-      game.load.image(graphicAssets.asteroidLarge.name, graphicAssets.asteroidLarge.URL);
-      game.load.image(graphicAssets.asteroidMedium.name, graphicAssets.asteroidMedium.URL);
-      game.load.image(graphicAssets.asteroidSmall.name, graphicAssets.asteroidSmall.URL);
-      game.load.image(graphicAssets.bullet.name, graphicAssets.bullet.URL);
-      game.load.image(graphicAssets.ship.name, graphicAssets.ship.URL);
+    game.load.image(graphicAssets.asteroidLarge.name, graphicAssets.asteroidLarge.URL);
+    game.load.image(graphicAssets.asteroidMedium.name, graphicAssets.asteroidMedium.URL);
+    game.load.image(graphicAssets.asteroidSmall.name, graphicAssets.asteroidSmall.URL);
+    game.load.image(graphicAssets.bullet.name, graphicAssets.bullet.URL);
+    game.load.image(graphicAssets.ship.name, graphicAssets.ship.URL);
+  },
+  create: function() {
+    this.initGraphics();
+  },
+  update: function() {
+
+  },
+  initGraphics: function () {
+    this.shipSprite = game.add.sprite(shipProperties.startX,
+                                      shipProperties.startY,
+                                      graphicAssets.ship.name);
+    this.shipSprite.angle = -90; // rotate 90d cw
+    this.shipSprite.anchor.set(0.5, 0.5); // translate the sprite anchor point to its center
   }
 }
+
+var game = new Phaser.Game(
+  gameProperties.screenWidth,
+  gameProperties.screenHeight,
+  Phaser.AUTO,
+  'gameDiv'
+);
+game.state.add(states.game, gameState);
+game.state.start(states.game);
